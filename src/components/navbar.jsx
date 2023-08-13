7a6bf5fa8a064e79db4c37e9419469213ce43601
Judah Hooper <judahhooper@icloud.com>
2023-08-13 13:45:58 +0100

    import and add usestate

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,38 +1,40 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, {useState} from 'react';
 import {FaBars, FaTimes} from "react-icons/fa";
 
-export default function NavBar() {
+const NavBar = () => {
+
+const [nav, setNav] = useState(false);
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-green-800 text-gray-300'>
         <div>
-            <img src="" alt='Logo' ></img> hello
+            <img src="" alt='' ></img>
         </div>
-        <div>
-            <ul className='flex '>
+
+            <ul className='hidden md:flex'>
                 <li>About</li>
                 <li>Skills</li>
                 <li>Experience</li>
                 <li>Contact</li>
             </ul>
-        </div>
 
         {/* burger for mobile */}
-        <div className='hidden'><FaBars/></div>
+        <div className='md:hidden z-10'><FaBars/></div>
 
-        {/* mobile menu */}
-        <div className='hidden'>            
-            <ul>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Experience</li>
-                <li>Contact</li>
+        {/* mobile menu */}           
+            <ul className='absolute top-0 left-0 w-full h-screen bg-green-800 flex flex-col justify-center items-center'>
+                <li className='py-6 text-4xl'>About</li>
+                <li className='py-6 text-4xl'>Skills</li>
+                <li className='py-6 text-4xl'>Experience</li>
+                <li className='py-6 text-4xl'>Contact</li>
             </ul>
-        </div>
 
         {/* socials */}
-        <div></div>
+        <div className='hidden'></div>
 
     </div>
   )
 }; 
+
+export default NavBar;
