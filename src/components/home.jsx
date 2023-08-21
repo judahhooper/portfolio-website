dd15a1cfc2bc5240fe18cea704ee1c5d6f30260f
Judah Hooper <judahhooper@icloud.com>
2023-08-21 20:53:12 +0200

    home smooth scrolling

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {HiArrowNarrowRight} from "react-icons/hi";
 import TypeWriterEffect from "react-typewriter-effect";
+import { Link } from "react-scroll";
 
 
 
@@ -12,12 +13,13 @@ const Home = () => {
     <div className='max-w-[1000px] mx-auto px-8 flex-col justify-center h-full translate-y-[30%] top-[50] md:translate-x-[-25%] z-10'>
         <p className='text-[#BF5700] pt-4 font-bold'>My name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold] py-4'><TypeWriterEffect startDelay={100} text="Judah Hooper." typeSpeed={75} stop/></h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-slate-800 py-4'>I'm looking for work as a junior developer.</h2>
+        <h2 className='text-4xl sm:text-7xl font-bold text-slate-800 py-4'>I'm looking to work as a junior developer.</h2>
         <p className='text-slate-800 py-4 max-w-[700px]'>I'm a self taught dev, developing engaging and interesting web applications in my free time. Thanks for being here, please stay a while.</p>
         <div>
-        <button className='text-slate-800 group border-2 px-6 py-3 my-2 border-slate-200 flex items-center hover:bg-[#BF5700] hover:text-slate-200 hover:border-slate-700 '>See what I've made
+        <Link to="work" smooth={true} duration={500}><button className='text-slate-800 group border-2 px-6 py-3 my-2 border-slate-200 flex items-center hover:bg-[#BF5700] hover:text-slate-200 hover:border-slate-700 '>
+        See what I've made
         <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-2'/></span>
-        </button>
+        </button></Link>
     </div>
     </div>
     </div>
